7643c9d02c284b2ba7c97f1c8715ba911aacf64c
Aniruddh <adubge@gmail.com>
2024-03-16 13:03:11 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-row h-dvh">
+      hi
+    </div>
+  );
+};
+
+export default Home;
